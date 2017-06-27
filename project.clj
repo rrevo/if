87a07efca6b7e0eb3b58a33d332c8c5bf623cbc8
Rahul Revo <rahulrevo@gmail.com>
2017-06-26 19:41:43 -0700

    Added gpg info
    lein deploy uses gpg for signing

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/rrevo/if"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :signing {:gpg-key "rahulrevo@gmail.com"})
